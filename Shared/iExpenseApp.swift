4619d6eabceaa2429884b738d053a354aa6f159e
Owais Shaikh <theowaiszoot@gmail.com>
2022-07-21 20:42:44 +0530

    Initial Commit

---
+++ Shared/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
